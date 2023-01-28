88b96b5206cb22f332fafa3299f84f215a991e4a
bryaanls <bryanluccas@hotmail.com>
2023-01-28 11:57:58 -0300

    passagem de dados pelo DOM com JSON

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,21 @@
 
 const btn = document.querySelector('#send');
 const table = document.querySelector('.table');
+
 btn.addEventListener('click', (e) => {
-
-    e.preventDefault();
-    const getTeam1 = document.querySelector('#team1');
-    const getTeam2 = document.querySelector('#team2');
-    const team1 = getTeam1.value;
-    const team2 = getTeam2.value;
-
-    const teams = [team1, team2]
-    table.innerHTML += teams;
-
-    console.log(teams[1], teams[0]);
+    const teams = getTeams();
+    salvaTimes(teams);
 })
 
 
+function getTeams() {
+    const getTeam1 = document.querySelector('#team1');
+    const getTeam2 = document.querySelector('#team2');
+    const teams = [getTeam1.value, getTeam2.value]
+    return teams;
+}
 
-
-
-
-
-
-
-
+function salvaTimes(times) {
+    const teamJSON = JSON.stringify(times)
+    localStorage.setItem('times', teamJSON);
+}
